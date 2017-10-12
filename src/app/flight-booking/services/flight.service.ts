15c604ae2daada072a380f25347cbb6c8d48f6b8
ManfredSteyer <manfred.steyer@gmx.net>
2017-10-13 01:00:09 +0200

    first commit

---
+++ src/app/flight-booking/services/flight.service.ts
@@ -0,0 +1,63 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable, Inject } from '@angular/core';
+import { Http, Headers, URLSearchParams } from '@angular/http';
+import { BASE_URL } from '../../app.tokens';
+import { Observable } from 'rxjs/Observable';
+import { Flight } from '../../entities/flight';
+import { OAuthService } from 'angular-oauth2-oidc';
+
+@Injectable()
+export class FlightService {
+
+    public flights: Array<Flight> = [];
+
+    constructor(private oauthService: OAuthService,
+                private http: HttpClient,
+                @Inject(BASE_URL) private baseUrl: string) {
+    }
+
+    findById(id: string): Observable<Flight> {
+
+        const url = this.baseUrl + '/api/flight';
+        // let url = this.baseUrl + '/api/secureflight';
+        // let url = '/data/flights.json';
+
+        const headers = new HttpHeaders()
+                                .set('Accept', 'text/json');
+
+        const params = new HttpParams()
+                                .set('id', id);
+
+        return this.http.get<Flight>(url, {headers, params});
+
+    }
+
+    find(from: string, to: string): void {
+
+        // const url = this.baseUrl + '/api/flight';
+        const url = this.baseUrl + '/api/secureflight';
+        // let url = '/data/flights.json';
+
+        const headers = new HttpHeaders()
+                                .set('Accept', 'application/json');
+
+        // this.oauthService.hasValidAccessToken();
+        // this.oauthService.hasValidIdToken();
+
+        const params = new HttpParams()
+                                .set('from', from)
+                                .set('to', to);
+
+        this
+            .http
+            .get<Flight[]>(url, {headers, params})
+            .subscribe(
+                (flights) => {
+                    this.flights = flights;
+                },
+                (err) => {
+                    console.warn(err);
+                }
+            );
+    }
+}
